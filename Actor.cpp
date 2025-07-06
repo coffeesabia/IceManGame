a1e49aab79179b26eb1e59d919bd7325d792a046
coffeesabia <coffeesabia@gmail.com>
2025-07-05 19:23:58 -0700

    Update Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -158,7 +158,7 @@ void Boulder::doSomething()
         case WAITING:
         {
             m_waitTicks++;
-            if (m_waitTicks >= 30)
+            if (m_waitTicks >= 5)
             {
                 m_state = FALLING;
                 getWorld()->playSound(SOUND_FALLING_ROCK);
@@ -191,49 +191,17 @@ void Boulder::doSomething()
             }
 
             if (iceBelow) {
-                setDead(); // hit ice
                 return;
             }
 
             // Safe to move down
             moveTo(x, y - 1);
+            --y;
 
             // Optional: Annoy IceMan or Protesters within radius 3
             // You can add this later if needed
 
             break;
-            
-            
-            
-            /*If the Boulder is currently in the stable state, then it must check to see if there is
-             any Ice in the 4 squares immediately below it. For example, if the Boulder is at
-             location x=20,y=30, it would check squares (20,29), (21,29), (22,29) and (23,29)
-             for Ice. If there is any Ice below the Boulder, it does nothing – after all it’s a
-             dumb rock. However, if none of the 4 squares beneath the Boulder have any Ice,
-             then the Boulder must transition into a waiting state:
-             A. It must enter a waiting state for the next 30 ticks*/
-             
-             /*3. If the Boulder is in a waiting state and 30 ticks have elapsed, then it must
-             transition into a falling state and play the sound SOUND_FALLING_ROCK.
-             
-             4. If the Boulder is in a falling state, then:
-             A. It must continue to move downward one square during each tick until it
-             either (a) hits the bottom of the oil field (i.e., it tries to move to y=-1), (b)
-             runs into the top of another Boulder, or (c) it runs into Ice (i.e., by
-             moving down a square, the Boulder would overlap over one or more Ice
-             objects). When any of the above conditions are met the Boulder must set
-             its state to dead so it can be removed from the game at the end of the
-             current tick.
-             B. If the Boulder comes within a radius of 3 (i.e. less than or equal to 3
-             squares, so 2.99 would count) of any Protester(s) or the Iceman while
-             falling, it must cause 100 points of annoyance to those actors (effectively
-             completely annoying them in one fell swoop). The Boulder will continue
-             falling down as descri
-              bed above, even if it annoys one or more Protesters
-             on its way down (i.e., it won’t stop falling if it runs into a Protester).
-             However, if the Boulder annoys the Iceman, the player will instantly lose
-             a life*/
-            
         }
     }
 }
@@ -243,6 +211,18 @@ void GoldNugget::doSomething()
     if (!isAlive()) return;
     
     setVisible(true);
+    
+    Actor* a = getWorld()->findNearbyPickerUpper(this, 3);
+        if (a != nullptr && a->canPickThingsUp()) {
+            Agent* ag = dynamic_cast<Agent*>(a);
+            if (ag != nullptr) {
+                ag->addGold();
+                setVisible(false);
+                setDead();
+                getWorld()->playSound(SOUND_GOT_GOODIE);
+                getWorld()->increaseScore(10);
+            }
+        }
 }
 
 void OilBarrel::doSomething()
@@ -250,6 +230,18 @@ void OilBarrel::doSomething()
     if (!isAlive()) return;
     
     setVisible(true);
+    
+    Actor* a = getWorld()->findNearbyPickerUpper(this, 3);
+        if (a != nullptr && a->canPickThingsUp()) {
+            ActivatingObject* ag = dynamic_cast<ActivatingObject*>(a);
+            if (ag != nullptr) {
+                ag->addBarrels();
+                setVisible(false);
+                setDead();                
+                getWorld()->playSound(SOUND_FOUND_OIL);
+                getWorld()->increaseScore(1000);
+            }
+        }
 }
 
 SonarKit::SonarKit(int startX, int startY, StudentWorld* sw, bool isVisible, bool canPick): ActivatingObject(sw, startX, startY, IID_SONAR, SOUND_SONAR, false, true, true)
@@ -277,4 +269,12 @@ void WaterPool::doSomething()
     setVisible(true);
 }
 
+bool Agent::canPickThingsUp() const{
+    
+    
+    return false;
+}
+
 //end of Actor.cpp
+
+
